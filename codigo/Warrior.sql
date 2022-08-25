bee0ca89f138f6577a440b22f232655bfcc8b898
joaaquinhidalgo <103291969+joaaquinhidalgo@users.noreply.github.com>
2022-08-25 19:54:26 -0300

    Update Warrior.sql

--- codigo/Warrior.sql
+++ codigo/Warrior.sql
@@ -82,3 +82,17 @@ CREATE TABLE `Warrior`.`Competidor` (
     REFERENCES `Warrior`.`Usuario` (`id_Usuario`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+-- -----------------------------------------------------
+-- Table `Warrior`.`Foro`
+-- -----------------------------------------------------
+CREATE TABLE `Warrior`.`foro` (
+  `id` int(7) NOT NULL auto_increment,
+  `Usuario` varchar(200) NOT NULL default '',
+  `titulo` varchar(200) NOT NULL default '',
+  `mensaje` text NOT NULL,
+  `fecha` datetime NOT NULL default '0000-00-00 00:00:00',
+  `respuestas` int(11) NOT NULL default '0',
+  `identificador` int(7) NOT NULL default '0',
+  `ult_respuesta` datetime default NULL,
+  KEY `id` (`id`)
+) TYPE=MyISAM;
