c600f810d757eeb7a167dfd8f5085691fc563f02
joaaquinhidalgo <jhidalgourman@gmail.com>
2022-10-20 16:25:19 -0300

    actualizacion
    jueves 20/10 pre clase de base de datos

--- codigo/login/ConexionLoginBase.php
+++
@@ -1,6 +0,0 @@
-$username="joa";
-$password="joa";
-$database="Warrior";
-$mysqli = new mysqli("localhost", $joa, $joa, $Warrior);
-$mysqli->select_db($Warrior) or die( "Unable to select database");
-$mysqli->close();
