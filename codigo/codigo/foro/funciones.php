2d54813b323daa82063d5ff470961bee3a92c0c9
joaaquinhidalgo <jhidalgourman@gmail.com>
2022-10-23 22:16:28 -0300

    domingo 23/10
    pantallas para la presentacion

--- codigo/codigo/foro/funciones.php
+++
@@ -1,8 +0,0 @@
-<?php
-function parsearTags($mensaje)
-{
-    $mensaje = str_replace("[citar]", "<blockquote><hr width='100%' size='2'>", $mensaje);
-    $mensaje = str_replace("[/citar]", "<hr width='100%' size='2'></blockquote>", $mensaje);
-    return $mensaje;
-}
-?>
--- codigo/codigo/informacion.md
+++
@@ -1,1 +0,0 @@
-### En esta carpeta se encontraran todos los archivos referidos al codigo
