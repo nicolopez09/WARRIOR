8602a674c05512d1dd9d87c6e3a0e6abdae602bb
joaaquinhidalgo <jhidalgourman@gmail.com>
2022-11-05 18:55:22 -0300

    perfil
    apartado perfil desarrollado

---
+++ codigo/login/abmlogin.php
@@ -0,0 +1,32 @@
+<?php
+
+$usuario= $_GET['usuario'];
+
+$contrasenia= $_GET['contrasenia'];
+
+$nombreBADA = "id19615577_warrior";
+
+$direccion = "localhost";
+
+$link = mysqli_connect($direccion, "id19615577_root123", "Nq=h!9D>bAG@tQi4", $nombreBADA);
+
+$query = "SELECT * FROM Usuario where Nombre_Usuario = '$usuario' and Contraseña = '$contrasenia';";
+
+$resultado = mysqli_query($link, $query);
+
+$count = mysqli_num_rows($resultado);
+
+
+
+if ($count == 1){
+    
+    echo "<h1><center>usuario ya existente</center></h1>";
+
+    }
+
+
+else{
+    
+}
+
+ ?>
